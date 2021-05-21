07a161d885f69343112a91784933f62becd3d2f2
Jarno Rajahalme <jarno@isovalent.com>
2021-05-21 16:59:38 -0700

    test: Add HTTP visibility annotation to echo-other-node
    Add HTTP visibility annotation to the echo-other-node pod. With a new
    enough Cilium agent those annotations will show also when
    echo-other-node has an ingress policy. Test behavior is not changed,
    but we could change to verify the additional visibility when Cilium
    agent with the visibility annotation is released.
    
    To see the annotations use e.g.:
    
    $ cilium connectivity test --test echo --print-flows --debug
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- connectivity/check/deployment.go
+++ connectivity/check/deployment.go
@@ -48,6 +48,7 @@ type deploymentParameters struct {
 	Affinity       *corev1.Affinity
 	ReadinessProbe *corev1.Probe
 	Labels         map[string]string
+	Annotations    map[string]string
 }
 
 func newDeployment(p deploymentParameters) *appsv1.Deployment {
@@ -71,6 +72,7 @@ func newDeployment(p deploymentParameters) *appsv1.Deployment {
 						"name": p.Name,
 						"kind": p.Kind,
 					},
+					Annotations: make(map[string]string, len(p.Annotations)),
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
@@ -103,6 +105,9 @@ func newDeployment(p deploymentParameters) *appsv1.Deployment {
 
 	for k, v := range p.Labels {
 		dep.Spec.Template.ObjectMeta.Labels[k] = v
+	}
+	for k, v := range p.Annotations {
+		dep.Spec.Template.ObjectMeta.Annotations[k] = v
 	}
 
 	return dep
@@ -310,6 +315,7 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 					},
 				},
 				ReadinessProbe: newLocalReadinessProbe(8080, "/"),
+				Annotations:    map[string]string{"io.cilium.proxy-visibility": "<Ingress/8080/TCP/HTTP>"},
 			})
 
 			_, err = ct.clients.dst.CreateDeployment(ctx, ct.params.TestNamespace, echoOtherNodeDeployment, metav1.CreateOptions{})
