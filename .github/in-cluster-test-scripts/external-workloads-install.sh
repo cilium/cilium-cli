78ceaa97ad9ea6e2d9de403b1d103aee9f4d98b4
Maciej Kwiek <maciej@isovalent.com>
2021-06-09 13:31:18 +0200

    ci: wait for Cilium start before enabling clustermesh
    This change fixes a bug when clustermesh apiserver was created directly
    after Cilium pods were created which caused the apiserver pod to not be
    managed by Cilium
    
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- .github/in-cluster-test-scripts/external-workloads-install.sh
+++ .github/in-cluster-test-scripts/external-workloads-install.sh
@@ -4,6 +4,7 @@ set -x
 set -e
 
 cilium install --cluster-name "${CLUSTER_NAME}" --restart-unmanaged-pods=false --config monitor-aggregation=none --config tunnel=vxlan --native-routing-cidr="${CLUSTER_CIDR}"
+cilium status --wait
 
 cilium clustermesh enable
 cilium clustermesh status --wait --wait-duration 5m
