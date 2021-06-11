9f694872643076a8b56f915df1fbd9238fddc3a0
Nicolas Busseneau <nicolas@isovalent.com>
2021-06-11 19:08:33 +0200

    azure: refactor Azure install functions
    Since we were starting to have many methods called from the main install
    method, we add a single entry point for Azure install to call.
    
    This entry point is responsible for properly calling all Azure
    sub-functions and setting up Azure-specific objects.
    
    All function names have been prefixed/renamed for consistency depending
    at which level they operate:
    
    - The entry point uses `aks` to be consistent with `gke` functions.
    - Sub-functions use `azure` since they interact with Azure objects
      rather than AKS objects.
    
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- install/install.go
+++ install/install.go
@@ -1523,11 +1523,7 @@ func (k *K8sInstaller) Install(ctx context.Context) error {
 			return fmt.Errorf("missing Azure resource group name")
 		}
 
-		if err := k.retrieveSubscriptionID(ctx); err != nil {
-			return err
-		}
-
-		if err := k.createAzureServicePrincipal(ctx); err != nil {
+		if err := k.aksSetup(ctx); err != nil {
 			return err
 		}
 	}
