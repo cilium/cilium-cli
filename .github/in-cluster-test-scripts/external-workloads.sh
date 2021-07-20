482d5cb4649063133ce34249282097f4fb99522d
Nicolas Busseneau <nicolas@isovalent.com>
2021-07-20 21:50:51 +0200

    workflows: add missing Relay port-forward to external workloads testing
    We somehow missed that one, resulting in flows not being validated in
    connectivity test via the external workloads workflow.
    
    Signed-off-by: Nicolas Busseneau <nicolas@isovalent.com>

--- .github/in-cluster-test-scripts/external-workloads.sh
+++ .github/in-cluster-test-scripts/external-workloads.sh
@@ -3,5 +3,10 @@
 set -x
 set -e
 
+# Port forward Relay
+cilium hubble port-forward&
+sleep 10s
+[[ $(pgrep -f "cilium.*hubble.*port-forward|kubectl.*port-forward.*hubble-relay" | wc -l) == 2 ]]
+
 # Run connectivity test
 cilium connectivity test --debug --all-flows
