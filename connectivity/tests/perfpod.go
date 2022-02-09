78f00b679c276f76864f896d04be687c01b359ab
Joe Talerico <rook@isovalent.com>
2022-02-08 19:10:28 -0500

    Increase execution time
    UDP workload fixed
    
    TODO :
     - Determine best way to report results.

--- connectivity/tests/perfpod.go
+++ connectivity/tests/perfpod.go
@@ -44,7 +44,7 @@ func iperf(sip string, ctx context.Context, podname string, a *check.Action, sam
 	}
 	for i := 0; i < samples; i++ {
 		var r Result
-		exec := []string{"/usr/bin/iperf3", "-c", sip, "-t", "5", "-Z", "--no-delay", "-J"}
+		exec := []string{"/usr/bin/iperf3", "-c", sip, "-t", "60", "-Z", "--no-delay", "-J"}
 		if udp {
 			exec = []string{"/usr/bin/iperf3", "-u", "-c", sip, "-t", "5", "-Z", "--no-delay", "-J"}
 		}
@@ -55,11 +55,15 @@ func iperf(sip string, ctx context.Context, podname string, a *check.Action, sam
 			a.Fatal("unable to parse output from iperf3")
 		}
 		r.iteration = i
-		r.bps = payload["end"].(map[string]interface{})["sum_sent"].(map[string]interface{})["bits_per_second"]
 		if !udp {
+			r.bps = payload["end"].(map[string]interface{})["sum_sent"].(map[string]interface{})["bits_per_second"]
 			r.rt = payload["end"].(map[string]interface{})["sum_sent"].(map[string]interface{})["retransmits"]
+			results[fmt.Sprintf("%s-tcp-stream-%d", podname, i)] = r
+		} else {
+			r.bps = payload["end"].(map[string]interface{})["sum"].(map[string]interface{})["bits_per_second"]
+			results[fmt.Sprintf("%s-udp-stream-%d", podname, i)] = r
 		}
-		results[fmt.Sprintf("%s-tcp-stream-%d", podname, i)] = r
+
 	}
 	return results
 }
@@ -70,10 +74,10 @@ func (s *tcpPodToPod) Run(ctx context.Context, t *check.Test) {
 			t.NewAction(s, "iperf-tcp", &c, server).Run(func(a *check.Action) {
 				results := iperf(server.Pod.Status.PodIP, ctx, c.Pod.Name, a, 1, false)
 				for test, result := range results {
-					a.Logf("📄 Results for : %s", test)
-					a.Logf("✅ TCP Retransmissions (lower is better) : %d", int(result.rt.(float64)))
-					a.Logf("✅ TCP Stream Performance - Iteration %d : %f (", result.iteration, (result.bps).(float64)/1000000000.0)
-					a.Log()
+
+					fmt.Printf("\n📄 Results for : %s\n", test)
+					fmt.Printf("✅ TCP Retransmissions (lower is better) : %d\n", int(result.rt.(float64)))
+					fmt.Printf("✅ TCP Stream Performance - Iteration %d : %f(Mbps)\n", result.iteration, (result.bps).(float64)/1000000.0)
 				}
 			})
 		}
@@ -106,11 +110,11 @@ func (s *udpPodtoPod) Run(ctx context.Context, t *check.Test) {
 			t.NewAction(s, "iperf-udp", &c, server).Run(func(a *check.Action) {
 				results := iperf(server.Pod.Status.PodIP, ctx, c.Pod.Name, a, 1, true)
 				for test, result := range results {
-					a.Logf("📄 Results for : %s", test)
-					a.Logf("✅ UDP Stream Performance - Iteration %d : %f (", result.iteration, (result.bps).(float64)/1000000000.0)
-					a.Log()
+					fmt.Printf("\n📄 Results for : %s\n", test)
+					fmt.Printf("✅ UDP Stream Performance - Iteration %d : %f(Mbps)\n", result.iteration, (result.bps).(float64)/1000000.0)
 				}
 			})
 		}
 	}
+	t.Info("Message")
 }
