cilium/cilium-cli

commit f9b2356377adfe0bb10c45164c61cbb8870912de

sysdump: include own log messages in sysdump
Store the messages logged by `cilium sysdump` in the
`cilium-sysdump.log` file in the sysdump itself. This allows to analyze
potential issues with `cilium sysdump` itself without having to ask
users to retrieve a new sysdump.

Signed-off-by: Tobias Klauser <tobias@cilium.io>

sysdump/sysdump.go
@@ -26,6 +26,8 @@ import (
 	"github.com/cilium/cilium-cli/internal/utils"
 )
 
+const sysdumpLogFile = "cilium-sysdump.log"
+
 // Options groups together the set of options required to collect a sysdump.
 type Options struct {
 	// The labels used to target Cilium pods.
@@ -94,6 +96,10 @@ type Collector struct {
 	Client  KubernetesClient
 	Options Options
 	Pool    *workerpool.WorkerPool
+	// logFile is the log file for the sydump log messages.
+	logFile *os.File
+	// logWriter is the io.Writer used for logging.
+	logWriter io.Writer
 	// subtasksWg is used to wait for subtasks to be submitted to the pool before calling 'Drain'.
 	// It is required since we don't know beforehand how many sub-tasks will be created, as they depend on the number of Cilium/Hubble/... pods found by "main" tasks.
 	subtasksWg sync.WaitGroup
@@ -125,6 +131,7 @@ func NewCollector(k KubernetesClient, o Options, startTime time.Time) (*Collecto
 	if err = os.MkdirAll(c.sysdumpDir, dirMode); err != nil {
 		return nil, fmt.Errorf("failed to create temporary directory: %w", err)
 	}
+	c.setupLogging(o.Writer)
 	c.logDebug("Using %v as a temporary directory", c.sysdumpDir)
 
 	// Grab the Kubernetes nodes for the target cluster.
@@ -157,6 +164,28 @@ func NewCollector(k KubernetesClient, o Options, startTime time.Time) (*Collecto
 	c.logDebug("Restricting bugtool and logs collection to pods in %v", c.NodeList)
 
 	return &c, nil
+}
+
+// setupLogging sets up sysdump collector loggging.
+func (c *Collector) setupLogging(w io.Writer) error {
+	var err error
+	c.logFile, err = os.Create(filepath.Join(c.sysdumpDir, sysdumpLogFile))
+	if err != nil {
+		return fmt.Errorf("failed to create sysdump log file: %w", err)
+	}
+	// Log to stdout and to a file in the sysdump itself
+	c.logWriter = io.MultiWriter(w, c.logFile)
+	return nil
+}
+
+// teardownLogging flushes writes to the sysdump collector log file and closes it.
+func (c *Collector) teardownLogging() {
+	// flush writes to log file
+	c.logFile.Sync()
+	// ...and close the log file
+	c.logFile.Close()
+	// rewire logger for the remaining log messages which won't make it into to log file
+	c.logWriter = os.Stdout
 }
 
 // replaceTimestamp can be used to replace the special timestamp placeholder in file and directory names.
@@ -847,6 +876,8 @@ func (c *Collector) Run() error {
 		c.logWarn("Please note that depending on your Cilium version and installation options, this may be expected")
 	}
 
+	c.teardownLogging()
+
 	// Create the zip file in the current directory.
 	c.log("🗳 Compiling sysdump")
 	p, err := os.Getwd()
@@ -868,7 +899,7 @@ func (c *Collector) Run() error {
 }
 
 func (c *Collector) log(msg string, args ...interface{}) {
-	fmt.Fprintf(c.Options.Writer, msg+"\n", args...)
+	fmt.Fprintf(c.logWriter, msg+"\n", args...)
 }
 
 func (c *Collector) logDebug(msg string, args ...interface{}) {

internal/cli/cmd/sysdump.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -18,7 +19,7 @@ var (
 	sysdumpOptions = sysdump.Options{
 		LargeSysdumpAbortTimeout: sysdump.DefaultLargeSysdumpAbortTimeout,
 		LargeSysdumpThreshold:    sysdump.DefaultLargeSysdumpThreshold,
-		Writer:                   sysdump.DefaultWriter,
+		Writer:                   os.Stdout,
 	}
 )
 

sysdump/defaults.go
@@ -4,7 +4,6 @@
 package sysdump
 
 import (
-	"os"
 	"runtime"
 	"time"
 )
@@ -36,7 +35,4 @@ const (
 var (
 	// DefaultWorkerCount is initialized to the machine's available CPUs.
 	DefaultWorkerCount = runtime.NumCPU()
-
-	// DefaultWriter points to os.Stdout by default.
-	DefaultWriter = os.Stdout
 )

sysdump/sysdump_test.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"os"
 	"path"
 	"testing"
 	"time"
@@ -51,6 +52,8 @@ func (b *SysdumpSuite) TestSysdumpCollector(c *check.C) {
 	c.Assert(path.Base(collector.sysdumpDir), check.Equals, "my-sysdump-"+timestamp)
 	tempFile := collector.AbsoluteTempPath("my-file-<ts>")
 	c.Assert(tempFile, check.Equals, path.Join(collector.sysdumpDir, "my-file-"+timestamp))
+	_, err = os.Stat(path.Join(collector.sysdumpDir, sysdumpLogFile))
+	c.Assert(err, check.IsNil)
 }
 
 func (b *SysdumpSuite) TestNodeList(c *check.C) {

commit 57590bb90cd55086d5455b393eb7740ae35babd3

sysdump: check (*Collector).setupLogging error
Fixes: f9b2356377ad ("sysdump: include own log messages in sysdump")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

sysdump/sysdump.go
@@ -131,7 +131,9 @@ func NewCollector(k KubernetesClient, o Options, startTime time.Time) (*Collecto
 	if err = os.MkdirAll(c.sysdumpDir, dirMode); err != nil {
 		return nil, fmt.Errorf("failed to create temporary directory: %w", err)
 	}
-	c.setupLogging(o.Writer)
+	if err = c.setupLogging(o.Writer); err != nil {
+		return nil, err
+	}
 	c.logDebug("Using %v as a temporary directory", c.sysdumpDir)
 
 	// Grab the Kubernetes nodes for the target cluster.