5b71b4971d3ed80eb3cd349826c8d8f315f74353
Jussi Maki <jussi@isovalent.com>
2022-08-05 09:28:53 +0200

    connectivity: Add test for cilium-health
    This ports the test test/k8s/health.go from the cilium repository
    for validating the connectivity between the cilium-agent instances
    and checking that the cilium-health command and health endpoints work
    correctly.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- connectivity/suite.go
+++ connectivity/suite.go
@@ -8,6 +8,7 @@ import (
 	_ "embed"
 
 	"github.com/blang/semver/v4"
+
 	"github.com/cilium/cilium/pkg/versioncheck"
 
 	"github.com/cilium/cilium-cli/connectivity/check"
@@ -239,6 +240,11 @@ func Run(ctx context.Context, ct *check.ConnectivityTest) error {
 			}
 			return check.ResultDrop, check.ResultNone
 		})
+
+	// Health check tests.
+	ct.NewTest("health").
+		WithFeatureRequirements(check.RequireFeatureEnabled(check.FeatureHealthChecking)).
+		WithScenarios(tests.CiliumHealth())
 
 	// The following tests have DNS redirect policies. They should be executed last.
 
