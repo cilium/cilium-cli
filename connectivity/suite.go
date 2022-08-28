16ff88240720d7207424e616f47519ce7f09a346
Tam Mach <tam.mach@cilium.io>
2022-08-29 00:31:50 +1000

    connectivity: Add all entities deny test
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- connectivity/suite.go
+++ connectivity/suite.go
@@ -29,6 +29,9 @@ var (
 	//go:embed manifests/deny-all-ingress.yaml
 	denyAllIngressPolicyYAML string
 
+	//go:embed manifests/deny-all-entities.yaml
+	denyAllEntitiesPolicyYAML string
+
 	//go:embed manifests/allow-all-except-world.yaml
 	allowAllExceptWorldPolicyYAML string
 	//go:embed manifests/allow-all-except-world-pre-v1.11.yaml
@@ -219,6 +222,17 @@ func Run(ctx context.Context, ct *check.ConnectivityTest) error {
 		WithScenarios(
 			tests.PodToPod(),
 			tests.PodToPodWithEndpoints(),
+		).
+		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
+			return check.ResultDrop, check.ResultNone
+		})
+
+	// This policy denies all entities by default
+	ct.NewTest("all-entities-deny").
+		WithPolicy(denyAllEntitiesPolicyYAML).
+		WithScenarios(
+			tests.PodToPod(),
+			tests.PodToCIDR(),
 		).
 		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
 			return check.ResultDrop, check.ResultNone
