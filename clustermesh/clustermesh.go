cilium/cilium-cli

commit dcfb77269f4887d4f6aebbf19bf58e8ea3b737d1

clustermesh: Fix --bpf-lb-sock on < 1.12
In v1.12 we renamed the --enable-host-reachable-services to the
--bpf-lb-sock, and then changed the name of the flag in "cilium-cli
clustermesh" cmd. This breaks the installations on < 1.12 clusters.

Make the derivation of the flag based on the Cilium vsn.

Reported-by: Jarno Rajahalme <jarno@isovalent.com>
Signed-off-by: Martynas Pumputis <m@lambda.lt>

clustermesh/clustermesh.go
@@ -24,6 +24,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
+	"github.com/blang/semver/v4"
 	"github.com/cilium/cilium/api/v1/models"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 
@@ -1482,7 +1483,7 @@ endpoints:
 - https://clustermesh-apiserver.cilium.io:$CLUSTER_PORT
 EOF
 
-CILIUM_OPTS=" --join-cluster --bpf-lb-sock --enable-endpoint-health-checking=false"
+CILIUM_OPTS=" --join-cluster %[8]s --enable-endpoint-health-checking=false"
 CILIUM_OPTS+=" --kvstore etcd --kvstore-opt etcd.config=/var/lib/cilium/etcd/config.yaml"
 if [ -n "$HOST_IP" ] ; then
     CILIUM_OPTS+=" --ipv4-node $HOST_IP"
@@ -1623,12 +1624,38 @@ func (k *K8sClusterMesh) WriteExternalWorkloadInstallScript(ctx context.Context,
 		k.params.Retries = 1
 	}
 
+	vsn, err := k.client.GetRunningCiliumVersion(ctx, k.params.Namespace)
+	if err != nil {
+		return err
+	}
+	sockLBOpt, err := getSockLBOpt(vsn)
+	if err != nil {
+		return err
+	}
+
 	fmt.Fprintf(writer, installScriptFmt,
 		daemonSet.Spec.Template.Spec.Containers[0].Image, clusterAddr,
 		configOverwrites,
 		string(ai.CA), string(ai.ExternalWorkloadCert), string(ai.ExternalWorkloadKey),
-		strconv.Itoa(k.params.Retries))
+		strconv.Itoa(k.params.Retries), sockLBOpt)
 	return nil
+}
+
+func getSockLBOpt(ciliumVSN string) (string, error) {
+	sockLBOpt := "--bpf-lb-sock"
+
+	vsn, err := semver.Parse(strings.TrimLeft(ciliumVSN, "v"))
+	if err != nil {
+		return "", err
+	}
+
+	vsn112 := semver.MustParse("1.12.0")
+	// Before 1.12, the socket LB was enabled via --enable-host-reachable-services flag
+	if vsn.LT(vsn112) {
+		sockLBOpt = "--enable-host-reachable-services"
+	}
+
+	return sockLBOpt, nil
 }
 
 func formatCEW(cew ciliumv2.CiliumExternalWorkload) string {

commit ca07a0a710e5c1b06246d77afde87037070b842f

clustermesh: use Cilium version extracted from helm secret if available
This fixes the following error in the external workload workflow in
cilium/cilium CI:

    Error: Unable to create external workload install script: No Major.Minor.Patch elements found

See https://github.com/cilium/cilium/pull/22441#issuecomment-1332086537

For https://github.com/cilium/cilium/issues/22451
Fixes: dcfb77269f48 ("clustermesh: Fix --bpf-lb-sock on < 1.12")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

clustermesh/clustermesh.go
@@ -27,9 +27,11 @@ import (
 	"github.com/blang/semver/v4"
 	"github.com/cilium/cilium/api/v1/models"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/versioncheck"
 
 	"github.com/cilium/cilium-cli/defaults"
 	"github.com/cilium/cilium-cli/internal/certs"
+	"github.com/cilium/cilium-cli/internal/helm"
 	"github.com/cilium/cilium-cli/internal/utils"
 	"github.com/cilium/cilium-cli/k8s"
 	"github.com/cilium/cilium-cli/status"
@@ -429,6 +431,7 @@ type k8sClusterMeshImplementation interface {
 	ListCiliumEndpoints(ctx context.Context, namespace string, options metav1.ListOptions) (*ciliumv2.CiliumEndpointList, error)
 	GetPlatform(ctx context.Context) (*k8s.Platform, error)
 	CiliumLogs(ctx context.Context, namespace, pod string, since time.Time, filter *regexp.Regexp) (string, error)
+	GetHelmState(ctx context.Context, namespace string, secretName string) (*helm.State, error)
 }
 
 type K8sClusterMesh struct {
@@ -462,6 +465,7 @@ type Parameters struct {
 	All                  bool
 	ConfigOverwrites     []string
 	Retries              int
+	HelmValuesSecretName string
 }
 
 func (p Parameters) validateParams() error {
@@ -1624,13 +1628,28 @@ func (k *K8sClusterMesh) WriteExternalWorkloadInstallScript(ctx context.Context,
 		k.params.Retries = 1
 	}
 
-	vsn, err := k.client.GetRunningCiliumVersion(ctx, k.params.Namespace)
+	var ciliumVer semver.Version
+
+	helmState, err := k.client.GetHelmState(ctx, k.params.Namespace, k.params.HelmValuesSecretName)
 	if err != nil {
-		return err
+		// Try to retrieve version from image tag
+		v, err := k.client.GetRunningCiliumVersion(ctx, k.params.Namespace)
+		if err != nil {
+			return err
+		}
+		ciliumVer, err = utils.ParseCiliumVersion(v)
+		if err != nil {
+			return fmt.Errorf("failed to parse Cilium version %s: %w", v, err)
+		}
+
+	} else {
+		ciliumVer = helmState.Version
 	}
-	sockLBOpt, err := getSockLBOpt(vsn)
-	if err != nil {
-		return err
+
+	sockLBOpt := "--bpf-lb-sock"
+	if ciliumVer.LT(versioncheck.MustVersion("1.12.0")) {
+		// Before 1.12, the socket LB was enabled via --enable-host-reachable-services flag
+		sockLBOpt = "--enable-host-reachable-services"
 	}
 
 	fmt.Fprintf(writer, installScriptFmt,
@@ -1639,23 +1658,6 @@ func (k *K8sClusterMesh) WriteExternalWorkloadInstallScript(ctx context.Context,
 		string(ai.CA), string(ai.ExternalWorkloadCert), string(ai.ExternalWorkloadKey),
 		strconv.Itoa(k.params.Retries), sockLBOpt)
 	return nil
-}
-
-func getSockLBOpt(ciliumVSN string) (string, error) {
-	sockLBOpt := "--bpf-lb-sock"
-
-	vsn, err := semver.Parse(strings.TrimLeft(ciliumVSN, "v"))
-	if err != nil {
-		return "", err
-	}
-
-	vsn112 := semver.MustParse("1.12.0")
-	// Before 1.12, the socket LB was enabled via --enable-host-reachable-services flag
-	if vsn.LT(vsn112) {
-		sockLBOpt = "--enable-host-reachable-services"
-	}
-
-	return sockLBOpt, nil
 }
 
 func formatCEW(cew ciliumv2.CiliumExternalWorkload) string {

install/install.go
@@ -203,6 +203,7 @@ type k8sInstallerImplementation interface {
 	DeleteIngressClass(ctx context.Context, name string, opts metav1.DeleteOptions) error
 	CiliumLogs(ctx context.Context, namespace, pod string, since time.Time, filter *regexp.Regexp) (string, error)
 	ListAPIResources(ctx context.Context) ([]string, error)
+	GetHelmState(ctx context.Context, namespace string, secretName string) (*helm.State, error)
 }
 
 type K8sInstaller struct {

internal/cli/cmd/clustermesh.go
@@ -14,6 +14,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cilium/cilium-cli/clustermesh"
+	"github.com/cilium/cilium-cli/defaults"
 )
 
 func newCmdClusterMesh() *cobra.Command {
@@ -297,6 +298,8 @@ func newCmdExternalWorkloadInstall() *cobra.Command {
 	cmd.Flags().DurationVar(&params.WaitDuration, "wait-duration", 15*time.Minute, "Maximum time to wait")
 	cmd.Flags().StringSliceVar(&params.ConfigOverwrites, "config", []string{}, "Cilium agent config entries (key=value)")
 	cmd.Flags().IntVar(&params.Retries, "retries", 4, "Number of Cilium agent start retries")
+
+	cmd.Flags().StringVar(&params.HelmValuesSecretName, "helm-values-secret-name", defaults.HelmValuesSecretName, "Secret name to store the auto-generated helm values file. The namespace is the same as where Cilium will be installed")
 
 	return cmd
 }