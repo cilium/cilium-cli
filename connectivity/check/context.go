cac6a199dd38c6bc218a79b9d746d8ea68405d1b
Tobias Klauser <tobias@cilium.io>
2022-12-12 14:17:43 +0100

    connectivity: allow to specify connect and request timeout
    These are used to set curl --max-time and --connect-timeout, and ping -w
    and -W, respectively. The default values are 20s for --request-timeout
    and 5s for --connect-timeout.
    
    Example usage:
        $ cilium connectivity test --request-timeout 5s --connect-timeout 2s
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- connectivity/check/context.go
+++ connectivity/check/context.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 	"time"
 
@@ -558,9 +559,16 @@ func (ct *ConnectivityTest) CurlCommand(peer TestPeer, opts ...string) []string 
 	cmd := []string{"curl",
 		"-w", "%{local_ip}:%{local_port} -> %{remote_ip}:%{remote_port} = %{response_code}",
 		"--silent", "--fail", "--show-error",
-		"--connect-timeout", "5",
 		"--output", "/dev/null",
 	}
+
+	if connectTimeout := ct.params.ConnectTimeout.Seconds(); connectTimeout > 0.0 {
+		cmd = append(cmd, "--connect-timeout", strconv.FormatFloat(connectTimeout, 'f', -1, 64))
+	}
+	if requestTimeout := ct.params.RequestTimeout.Seconds(); requestTimeout > 0.0 {
+		cmd = append(cmd, "--max-time", strconv.FormatFloat(requestTimeout, 'f', -1, 64))
+	}
+
 	cmd = append(cmd, opts...)
 	cmd = append(cmd, fmt.Sprintf("%s://%s%s",
 		peer.Scheme(),
@@ -570,7 +578,17 @@ func (ct *ConnectivityTest) CurlCommand(peer TestPeer, opts ...string) []string 
 }
 
 func (ct *ConnectivityTest) PingCommand(peer TestPeer) []string {
-	return []string{"ping", "-w", "3", "-c", "1", peer.Address()}
+	cmd := []string{"ping", "-c", "1"}
+
+	if connectTimeout := ct.params.ConnectTimeout.Seconds(); connectTimeout > 0.0 {
+		cmd = append(cmd, "-W", strconv.FormatFloat(connectTimeout, 'f', -1, 64))
+	}
+	if requestTimeout := ct.params.RequestTimeout.Seconds(); requestTimeout > 0.0 {
+		cmd = append(cmd, "-w", strconv.FormatFloat(requestTimeout, 'f', -1, 64))
+	}
+
+	cmd = append(cmd, peer.Address())
+	return cmd
 }
 
 func (ct *ConnectivityTest) RandomClientPod() *Pod {
