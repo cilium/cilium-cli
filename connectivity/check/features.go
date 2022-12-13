cilium/cilium-cli

commit 976cf82fd7525d793696bd17d12c74edfec3dd34

connectivity: Add feature to detect tunneling mode
Going to be used by the upcoming encryption tests.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

connectivity/check/features.go
@@ -26,6 +26,7 @@ const (
 	FeatureL7Proxy            Feature = "l7-proxy"
 	FeatureHostFirewall       Feature = "host-firewall"
 	FeatureICMPPolicy         Feature = "icmp-policy"
+	FeatureTunnel             Feature = "tunnel"
 
 	FeatureKPRMode                Feature = "kpr-mode"
 	FeatureKPRExternalIPs         Feature = "kpr-external-ips"
@@ -159,6 +160,15 @@ func (ct *ConnectivityTest) extractFeaturesFromConfigMap(ctx context.Context, cl
 	}
 	result[FeatureCNIChaining] = FeatureStatus{
 		Enabled: mode != "none",
+		Mode:    mode,
+	}
+
+	mode = "Disabled"
+	if v, ok := cm.Data["tunnel"]; ok {
+		mode = v
+	}
+	result[FeatureTunnel] = FeatureStatus{
+		Enabled: mode != "Disabled",
 		Mode:    mode,
 	}
 

commit 8bbf16fe3badd458dee6d04d01d1d793f3dca89f

connectivity: Fix tunnel feature detection
It's "disabled, not "Disabled".

Fixes: 976cf82fd ("connectivity: Add feature to detect tunneling mode")
Reported-by: Tobias Klauser <tobias@cilium.io>
Signed-off-by: Martynas Pumputis <m@lambda.lt>

connectivity/check/features.go
@@ -163,12 +163,12 @@ func (ct *ConnectivityTest) extractFeaturesFromConfigMap(ctx context.Context, cl
 		Mode:    mode,
 	}
 
-	mode = "Disabled"
+	mode = "disabled"
 	if v, ok := cm.Data["tunnel"]; ok {
 		mode = v
 	}
 	result[FeatureTunnel] = FeatureStatus{
-		Enabled: mode != "Disabled",
+		Enabled: mode != "disabled",
 		Mode:    mode,
 	}
 