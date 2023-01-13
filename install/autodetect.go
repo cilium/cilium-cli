cilium/cilium-cli

commit 31453a5469effff922a696f6afec3c19f75ec229

install: Auto-enable BPF masquerade
By default, the BPF-based masquerading is disabled. Enable the feature
if the KPR=strict and a user haven't specified the helm's
"bpf.masquerade" option.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

install/autodetect.go
@@ -170,6 +170,7 @@ func (k *K8sInstaller) autodetectAndValidate(ctx context.Context) error {
 	}
 
 	k.autodetectKubeProxy(ctx)
+	k.autoEnableBPFMasq()
 	return nil
 }
 
@@ -236,5 +237,27 @@ func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context) error {
 			fmt.Sprintf("k8sServiceHost=%s", apiServerHost),
 			fmt.Sprintf("k8sServicePort=%s", apiServerPort))
 	}
+
 	return nil
+}
+
+func (k *K8sInstaller) autoEnableBPFMasq() {
+	// Auto-enable BPF masquerading if KPR=strict
+	foundKPRStrict := k.params.KubeProxyReplacement == "strict"
+	foundMasq := false
+	for _, param := range k.params.HelmOpts.Values {
+		if !foundKPRStrict && param == "kubeProxyReplacement=strict" {
+			foundKPRStrict = true
+			continue
+		}
+		if strings.HasPrefix(param, "bpf.masquerade") {
+			foundMasq = true
+			break
+		}
+	}
+
+	if foundKPRStrict && !foundMasq {
+		k.params.HelmOpts.Values = append(k.params.HelmOpts.Values,
+			"bpf.masquerade=true")
+	}
 }

commit eee5df19c86699d2dd0ef6408df2876ad2019a41

install: Do not enable BPF masq if ipv6 is enabled
Until [1] has been resolved, we need to disable BPF masq.

[1]: https://github.com/cilium/cilium/issues/14350

Fixes: 31453a5469 ("install: Auto-enable BPF masquerade")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

install/autodetect.go
@@ -242,9 +242,10 @@ func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context) error {
 }
 
 func (k *K8sInstaller) autoEnableBPFMasq() {
-	// Auto-enable BPF masquerading if KPR=strict
+	// Auto-enable BPF masquerading if KPR=strict and IPv6=disabled
 	foundKPRStrict := k.params.KubeProxyReplacement == "strict"
 	foundMasq := false
+	enabledIPv6 := false
 	for _, param := range k.params.HelmOpts.Values {
 		if !foundKPRStrict && param == "kubeProxyReplacement=strict" {
 			foundKPRStrict = true
@@ -254,9 +255,13 @@ func (k *K8sInstaller) autoEnableBPFMasq() {
 			foundMasq = true
 			break
 		}
+		if strings.HasPrefix(param, "ipv6.enabled=true") {
+			enabledIPv6 = true
+			break
+		}
 	}
 
-	if foundKPRStrict && !foundMasq {
+	if foundKPRStrict && !foundMasq && !enabledIPv6 {
 		k.params.HelmOpts.Values = append(k.params.HelmOpts.Values,
 			"bpf.masquerade=true")
 	}