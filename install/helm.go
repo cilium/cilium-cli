cilium/cilium-cli

commit 64924bfa96632bb43abae6ecfc3f7a5260a8d2ba

enabled l7Proxy & removed err.log message for WireGuard
Signed-off-by: Pratyay Banerjee <putubanerjee23@gmail.com>

install/helm.go
@@ -125,11 +125,13 @@ func (k *K8sInstaller) generateManifests(ctx context.Context) error {
 			// TODO(gandro): Future versions of Cilium will remove the following
 			// two limitations, we will need to have set the config map values
 			// based on the installed Cilium version
-			helmMapOpts["l7Proxy"] = "false"
-			k.Log("ℹ️  L7 proxy disabled due to Wireguard encryption")
+			if versioncheck.MustCompile("<=1.13.0")(k.chartVersion) {
+				helmMapOpts["l7Proxy"] = "false"
+				k.Log("ℹ️  L7 proxy disabled due to Wireguard encryption")
 
-			if k.params.NodeEncryption {
-				k.Log("⚠️️  Wireguard does not support node encryption yet")
+				if k.params.NodeEncryption {
+					k.Log("⚠️️  Wireguard does not support node encryption yet")
+				}
 			}
 		}
 

commit 78f0bf86e4d4f1032b6d44a022ab3a0bd819b3e5

fixes versioncheck for l7 proxy disabling with wireguard encryption
This commit fixes versioncheck so that it includes patch versions of cilium 1.13.x. Versions 1.13.x triggers en error:

level=fatal msg="Wireguard (--enable-wireguard) is not compatible with L7 proxy (--enable-l7-proxy)" subsys=daemon

Fixes: 64924bfa9663 ("enabled l7Proxy & removed err.log message for WireGuard")

Signed-off-by: Tomas Leypold <tomas@leypold.cz>

install/helm.go
@@ -159,7 +159,7 @@ func (k *K8sInstaller) getHelmValues() (map[string]interface{}, error) {
 			// TODO(gandro): Future versions of Cilium will remove the following
 			// two limitations, we will need to have set the config map values
 			// based on the installed Cilium version
-			if versioncheck.MustCompile("<=1.13.0")(k.chartVersion) {
+			if versioncheck.MustCompile("<1.14.0")(k.chartVersion) {
 				helmMapOpts["l7Proxy"] = "false"
 				k.Log("ℹ️  L7 proxy disabled due to Wireguard encryption")
 