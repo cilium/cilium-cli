cilium/cilium-cli

commit e7e739c7b72b5565a18723b19924762b4bfe8b3c

connectivity: Install podCIDR => nodeIP on non-Cilium nodes
This commit moves the installation of the podCIDR => nodeIP routes in
nodes which do not run Cilium (--nodes-without-cilium) to the CLI. The
routes are needed for "from-cidr" tests.

The main motivation for the move is that this significantly simplifies
running the aforementioned tests. Users are no longer required to
manually install those routes before running the tests. Also, it will
allow us to remove lots of boilerplate from the GH actions which run
those tests.

There was some concern regarding a safety aspect. I.e., the move makes
the CLI to modify a cluster state. In my opinion that concern can be
refuted by stating that the modification happens only on designated
nodes which do not run Cilium, and those nodes are meant only for
testing. In addition, a subsequent commit will protect the execution of
those tests which install the routes with "--include-unsafe-tests".

Signed-off-by: Martynas Pumputis <m@lambda.lt>

connectivity/check/test.go
@@ -66,6 +66,10 @@ type Test struct {
 	// for this test to be run
 	requirements []FeatureRequirement
 
+	// installIPRoutesFromOutsideToPodCIDRs indicates that the test runner needs
+	// to install podCIDR => nodeIP routes before running the test
+	installIPRoutesFromOutsideToPodCIDRs bool
+
 	// Scenarios registered to this test.
 	scenarios map[Scenario][]*Action
 
@@ -158,6 +162,14 @@ func (t *Test) setup(ctx context.Context) error {
 		t.ciliumLogs(ctx)
 		return fmt.Errorf("applying network policies: %w", err)
 	}
+
+	if err := t.Context().modifyStaticRoutesForNodesWithoutCilium(ctx, "add"); err != nil {
+		return fmt.Errorf("installing static routes: %w", err)
+	}
+
+	t.finalizers = append(t.finalizers, func() error {
+		return t.Context().modifyStaticRoutesForNodesWithoutCilium(ctx, "del")
+	})
 
 	return nil
 }
@@ -474,6 +486,15 @@ func (t *Test) WithFeatureRequirements(reqs ...FeatureRequirement) *Test {
 			t.requirements = append(t.requirements, target)
 		}
 	}
+
+	return t
+}
+
+// WithIPRoutesFromOutsideToPodCIDRs instructs the test runner that
+// podCIDR => nodeIP routes needs to be installed on a node which doesn't run
+// Cilium before running the test (and removed after the test completion).
+func (t *Test) WithIPRoutesFromOutsideToPodCIDRs() *Test {
+	t.installIPRoutesFromOutsideToPodCIDRs = true
 
 	return t
 }

.github/workflows/kind.yaml
@@ -73,59 +73,6 @@ jobs:
             kubectl label nodes "${node}" cilium.io/no-schedule=true
           done
 
-      # This is needed for the tests that run with nodes without Cilium.
-      - name: Install static routes
-        run: |
-          EXTERNAL_FROM_CIDRS=($(kubectl get nodes -o jsonpath='{range .items[*]}{.spec.podCIDR}{"\n"}{end}'))
-          EXTERNAL_NODE_IPS=() # Nodes IPs are collected to be passed to the Cilium CLI later on.
-
-          # Loop over each pod CIDR from all nodes.
-          for i in "${!EXTERNAL_FROM_CIDRS[@]}"; do
-            EXTERNAL_FROM_CIDR=${EXTERNAL_FROM_CIDRS[i]}
-
-            if [[ $EXTERNAL_FROM_CIDR =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+$ ]]; then
-              IP_FAMILY="v4"
-            elif [[ $EXTERNAL_FROM_CIDR =~ ^[0-9a-fA-F:]+/[0-9]+$ ]]; then
-              IP_FAMILY="v6"
-            else
-              echo "ERROR: Malformed pod CIDR '${EXTERNAL_FROM_CIDR}'" >&2
-              exit 1
-            fi
-
-            IFS=',' read -ra NODES_WITHOUT <<< "$NODES_WITHOUT_CILIUM" # Split by comma into an array
-            for WITHOUT in "${NODES_WITHOUT[@]}"; do
-              # Fetch node with a specific pod CIDR.
-              node=$(kubectl get nodes -o jsonpath="{range .items[?(@.spec.podCIDR == '$EXTERNAL_FROM_CIDR')]}{@.metadata.name}{end}")
-              if [[ -z "$node" ]]; then
-                echo "ERROR: Could not find node with .spec.podCIDR matching ${EXTERNAL_FROM_CIDR}" >&2
-                exit 1
-              fi
-              for NODE_IP in $(kubectl get node "$node" -o jsonpath="{.status.addresses[?(@.type == 'InternalIP')].address}"); do
-                # Skip if the node IP's family mismatches with pod CIDR's
-                # family. Cannot create a route with the gateway IP family
-                # mismatching the subnet.
-                if [[ "$IP_FAMILY" == "v4" && ! "$NODE_IP" =~ \. ]]; then
-                  continue
-                elif [[ "$IP_FAMILY" == "v6" && ! "$NODE_IP" =~ \: ]]; then
-                  continue
-                fi
-                # Install static route on the host, towards the pod CIDR via the node IP.
-                docker exec "$WITHOUT" ip route replace "${EXTERNAL_FROM_CIDR}" via "${NODE_IP}"
-                EXTERNAL_NODE_IPS+=("${NODE_IP}")
-              done
-            done
-          done
-
-          # Join the elements with a comma delimiter, or leave them unmodified
-          # if there's only one element so that it can be passed properly to
-          # the CLI.
-          if [[ ${#EXTERNAL_NODE_IPS[@]} -eq 1 ]]; then
-            EXTERNAL_NODE_IPS_PARAM="${EXTERNAL_NODE_IPS[0]}"
-          else
-            EXTERNAL_NODE_IPS_PARAM=$(IFS=','; echo "${EXTERNAL_NODE_IPS[*]}")
-          fi
-          echo "EXTERNAL_NODE_IPS_PARAM=${EXTERNAL_NODE_IPS_PARAM}" >> $GITHUB_ENV
-
       # Install Cilium with HostPort support and enables Prometheus for extended connectivity test.
       - name: Install Cilium
         run: |
@@ -154,7 +101,6 @@ jobs:
         run: |
           # Run the connectivity test in non-default namespace (i.e. not cilium-test)
           cilium connectivity test --debug --all-flows --test-namespace test-namespace \
-            --external-from-cidrs="${EXTERNAL_NODE_IPS_PARAM}" \
             --collect-sysdump-on-failure --junit-file connectivity-${{ matrix.mode }}.xml
 
       - name: Upload junit output
@@ -205,7 +151,6 @@ jobs:
       - name: Connectivity test
         run: |
           cilium connectivity test --debug --force-deploy --all-flows --test-namespace test-namespace \
-            --external-from-cidrs="${EXTERNAL_NODE_IPS_PARAM}" \
             --collect-sysdump-on-failure --junit-file connectivity-ipsec-${{ matrix.mode }}.xml
 
       - name: Upload junit output

connectivity/check/check.go
@@ -55,8 +55,8 @@ type Parameters struct {
 	ExternalCIDR          string
 	ExternalIP            string
 	ExternalOtherIP       string
-	ExternalFromCIDRs     []string
-	ExternalFromCIDRMasks []int // Derived from ExternalFromCIDRs
+	PodCIDRs              []podCIDRs
+	NodesWithoutCiliumIPs []nodesWithoutCiliumIP
 	JunitFile             string
 
 	K8sVersion           string
@@ -73,6 +73,16 @@ type Parameters struct {
 
 	CollectSysdumpOnFailure bool
 	SysdumpOptions          sysdump.Options
+}
+
+type podCIDRs struct {
+	CIDR   string
+	HostIP string
+}
+
+type nodesWithoutCiliumIP struct {
+	IP   string
+	Mask int
 }
 
 func (p Parameters) ciliumEndpointTimeout() time.Duration {

connectivity/check/context.go
@@ -308,8 +308,12 @@ func (ct *ConnectivityTest) SetupAndValidate(ctx context.Context) error {
 		}
 	}
 	if ct.features.MatchRequirements(RequireFeatureEnabled(FeatureNodeWithoutCilium)) {
-		if err := ct.validateExternalFromCIDRsWithNodesWithoutCilium(); err != nil {
-			return fmt.Errorf("invalid configuration for nodes without Cilium: %w", err)
+		if err := ct.detectPodCIDRs(ctx); err != nil {
+			return fmt.Errorf("unable to detect pod CIDRs: %w", err)
+		}
+
+		if err := ct.detectNodesWithoutCiliumIPs(); err != nil {
+			return fmt.Errorf("unable to detect nodes w/o Cilium IPs: %w", err)
 		}
 	}
 	return nil
@@ -544,17 +548,62 @@ func (ct *ConnectivityTest) enableHubbleClient(ctx context.Context) error {
 	return nil
 }
 
-func (ct *ConnectivityTest) validateExternalFromCIDRsWithNodesWithoutCilium() error {
-	if len(ct.params.ExternalFromCIDRs) == 0 {
-		ct.Fatalf("--%s must not be empty if Cilium was install with --%s set", "external-from-cidrs", "nodes-without-cilium")
+func (ct *ConnectivityTest) detectPodCIDRs(ctx context.Context) error {
+	nodes, err := ct.client.ListNodes(ctx, metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to list nodes: %w", err)
 	}
-	for _, cidr := range ct.params.ExternalFromCIDRs {
-		addr, err := netip.ParseAddr(cidr)
-		if err != nil {
-			return fmt.Errorf("unable to parse external-from-cidr %q: %w", cidr, err)
+
+	for _, n := range nodes.Items {
+		for _, cidr := range n.Spec.PodCIDRs {
+			f := GetIPFamily(ct.hostNetNSPodsByNode[n.Name].Pod.Status.HostIP)
+			if strings.Contains(cidr, ":") && f == IPFamilyV4 {
+				// Skip if the host IP of the pod mismatches with pod CIDR.
+				// Cannot create a route with the gateway IP family
+				// mismatching the subnet.
+				continue
+			}
+			hostIP := ct.hostNetNSPodsByNode[n.Name].Pod.Status.HostIP
+			ct.params.PodCIDRs = append(ct.params.PodCIDRs, podCIDRs{cidr, hostIP})
 		}
-		ct.params.ExternalFromCIDRMasks = append(ct.params.ExternalFromCIDRMasks, addr.BitLen())
 	}
+
+	return nil
+}
+
+func (ct *ConnectivityTest) detectNodesWithoutCiliumIPs() error {
+	for _, n := range ct.nodesWithoutCilium {
+		pod := ct.hostNetNSPodsByNode[n]
+		for _, ip := range pod.Pod.Status.PodIPs {
+			hostIP, err := netip.ParseAddr(ip.IP)
+			if err != nil {
+				return fmt.Errorf("unable to parse nodes without Cilium IP addr %q: %w", ip.IP, err)
+			}
+			ct.params.NodesWithoutCiliumIPs = append(ct.params.NodesWithoutCiliumIPs,
+				nodesWithoutCiliumIP{ip.IP, hostIP.BitLen()})
+		}
+	}
+
+	return nil
+}
+
+func (ct *ConnectivityTest) modifyStaticRoutesForNodesWithoutCilium(ctx context.Context, verb string) error {
+	for _, e := range ct.params.PodCIDRs {
+		for _, withoutCilium := range ct.nodesWithoutCilium {
+			pod := ct.hostNetNSPodsByNode[withoutCilium]
+			_, err := ct.client.ExecInPod(ctx, pod.Pod.Namespace, pod.Pod.Name, hostNetNSDeploymentName,
+				[]string{"ip", "route", verb, e.CIDR, "via", e.HostIP},
+			)
+			ct.Debugf("Modifying (%s) static route on nodes without Cilium (%v): %v",
+				verb, withoutCilium,
+				[]string{"ip", "route", verb, e.CIDR, "via", e.HostIP},
+			)
+			if err != nil {
+				return fmt.Errorf("failed to %s static route: %w", verb, err)
+			}
+		}
+	}
+
 	return nil
 }
 

connectivity/manifests/echo-ingress-from-cidr.yaml
@@ -9,6 +9,6 @@ spec:
       kind: echo
   ingress:
   - fromCIDR:
-{{ range $i, $cidr := .ExternalFromCIDRs }}
-    - {{$cidr}}/{{index $.ExternalFromCIDRMasks $i}}
+{{ range $i := .NodesWithoutCiliumIPs }}
+    - {{$i.IP}}/{{$i.Mask}}
 {{ end }}

connectivity/suite.go
@@ -247,10 +247,14 @@ func Run(ctx context.Context, ct *check.ConnectivityTest, addExtraTests func(*ch
 		tests.PodToExternalWorkload(),
 		tests.PodToCIDR(tests.WithRetryAll()),
 	}
-	if s, ok := ct.Feature(check.FeatureNodeWithoutCilium); ok && s.Enabled {
-		noPoliciesScenarios = append(noPoliciesScenarios, tests.FromCIDRToPod())
-	}
+
 	ct.NewTest("no-policies").WithScenarios(noPoliciesScenarios...)
+
+	// TODO(brb) --include-unsafe-tests
+	ct.NewTest("no-policies-from-outside").
+		WithFeatureRequirements(check.RequireFeatureEnabled(check.FeatureNodeWithoutCilium)).
+		WithIPRoutesFromOutsideToPodCIDRs().
+		WithScenarios(tests.FromCIDRToPod())
 
 	// Skip the nodeport-related tests in the multicluster scenario if KPR is not
 	// enabled, since global nodeport services are not supported in that case.
@@ -323,11 +327,21 @@ func Run(ctx context.Context, ct *check.ConnectivityTest, addExtraTests func(*ch
 	//    then when replies come back, they are considered as "replies" to the outbound connection.
 	//    so they are not subject to ingress policy.
 	allIngressDenyScenarios := []check.Scenario{tests.PodToPod(), tests.PodToCIDR(tests.WithRetryAll())}
-	if s, ok := ct.Feature(check.FeatureNodeWithoutCilium); ok && s.Enabled {
-		allIngressDenyScenarios = append(allIngressDenyScenarios, tests.FromCIDRToPod())
-	}
 	ct.NewTest("all-ingress-deny").WithCiliumPolicy(denyAllIngressPolicyYAML).
 		WithScenarios(allIngressDenyScenarios...).
+		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
+			if a.Destination().Address(check.GetIPFamily(ct.Params().ExternalOtherIP)) == ct.Params().ExternalOtherIP ||
+				a.Destination().Address(check.GetIPFamily(ct.Params().ExternalIP)) == ct.Params().ExternalIP {
+				return check.ResultOK, check.ResultNone
+			}
+			return check.ResultDrop, check.ResultDefaultDenyIngressDrop
+		})
+
+	// TODO(brb) --include-unsafe-tests
+	ct.NewTest("all-ingress-deny-from-outside").WithCiliumPolicy(denyAllIngressPolicyYAML).
+		WithFeatureRequirements(check.RequireFeatureEnabled(check.FeatureNodeWithoutCilium)).
+		WithIPRoutesFromOutsideToPodCIDRs().
+		WithScenarios(tests.FromCIDRToPod()).
 		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
 			if a.Destination().Address(check.GetIPFamily(ct.Params().ExternalOtherIP)) == ct.Params().ExternalOtherIP ||
 				a.Destination().Address(check.GetIPFamily(ct.Params().ExternalIP)) == ct.Params().ExternalIP {
@@ -417,11 +431,22 @@ func Run(ctx context.Context, ct *check.ConnectivityTest, addExtraTests func(*ch
 
 	// This policy allows ingress to echo only from client with a label 'other:client'.
 	echoIngressScenarios := []check.Scenario{tests.PodToPod()}
-	if s, ok := ct.Feature(check.FeatureNodeWithoutCilium); ok && s.Enabled {
-		echoIngressScenarios = append(echoIngressScenarios, tests.FromCIDRToPod())
-	}
 	ct.NewTest("echo-ingress").WithCiliumPolicy(echoIngressFromOtherClientPolicyYAML).
 		WithScenarios(echoIngressScenarios...).
+		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
+			if a.Destination().HasLabel("kind", "echo") && !a.Source().HasLabel("other", "client") {
+				// TCP handshake fails both in egress and ingress when
+				// L3(/L4) policy drops at either location.
+				return check.ResultDropCurlTimeout, check.ResultDropCurlTimeout
+			}
+			return check.ResultOK, check.ResultOK
+		})
+
+		// TODO(brb) --include-unsafe-tests
+	ct.NewTest("echo-ingress-from-outside").WithCiliumPolicy(echoIngressFromOtherClientPolicyYAML).
+		WithFeatureRequirements(check.RequireFeatureEnabled(check.FeatureNodeWithoutCilium)).
+		WithIPRoutesFromOutsideToPodCIDRs().
+		WithScenarios(tests.FromCIDRToPod()).
 		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
 			if a.Destination().HasLabel("kind", "echo") && !a.Source().HasLabel("other", "client") {
 				// TCP handshake fails both in egress and ingress when
@@ -544,16 +569,17 @@ func Run(ctx context.Context, ct *check.ConnectivityTest, addExtraTests func(*ch
 			return check.ResultOK, check.ResultNone
 		})
 
-	if s, ok := ct.Feature(check.FeatureNodeWithoutCilium); ok && s.Enabled {
-		ct.NewTest("from-cidr-host-netns").
-			WithCiliumPolicy(renderedTemplates["echoIngressFromCIDRYAML"]).
-			WithScenarios(
-				tests.FromCIDRToPod(),
-			).
-			WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
-				return check.ResultOK, check.ResultNone
-			})
-	}
+	// TODO(brb) --include-unsafe-tests
+	ct.NewTest("from-cidr-host-netns").
+		WithFeatureRequirements(check.RequireFeatureEnabled(check.FeatureNodeWithoutCilium)).
+		WithCiliumPolicy(renderedTemplates["echoIngressFromCIDRYAML"]).
+		WithIPRoutesFromOutsideToPodCIDRs().
+		WithScenarios(
+			tests.FromCIDRToPod(),
+		).
+		WithExpectations(func(a *check.Action) (egress, ingress check.Result) {
+			return check.ResultOK, check.ResultNone
+		})
 
 	// Tests with deny policy
 	ct.NewTest("echo-ingress-from-other-client-deny").

internal/cli/cmd/connectivity.go
@@ -133,7 +133,6 @@ func newCmdConnectivityTest(hooks Hooks) *cobra.Command {
 	cmd.Flags().StringVar(&params.ExternalCIDR, "external-cidr", "1.0.0.0/8", "CIDR to use as external target in connectivity tests")
 	cmd.Flags().StringVar(&params.ExternalIP, "external-ip", "1.1.1.1", "IP to use as external target in connectivity tests")
 	cmd.Flags().StringVar(&params.ExternalOtherIP, "external-other-ip", "1.0.0.1", "Other IP to use as external target in connectivity tests")
-	cmd.Flags().StringSliceVar(&params.ExternalFromCIDRs, "external-from-cidrs", []string{}, "CIDRs representing nodes without Cilium to be used in connectivity tests")
 	cmd.Flags().StringVar(&params.JunitFile, "junit-file", "", "Generate junit report and write to file")
 	cmd.Flags().BoolVar(&params.SkipIPCacheCheck, "skip-ip-cache-check", true, "Skip IPCache check")
 	cmd.Flags().MarkHidden("skip-ip-cache-check")

commit 33eb4a9829108fd8012cf7dd2c20eb953a06b13c

connectivity: Fix installing IP routes to nodes w/o Cilium
Previously, we unconditionally installed the IP routes.

Fixes: e7e739c7b ("connectivity: Install podCIDR => nodeIP on non-Cilium nodes")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

connectivity/check/test.go
@@ -168,13 +168,15 @@ func (t *Test) setup(ctx context.Context) error {
 		return fmt.Errorf("applying network policies: %w", err)
 	}
 
-	if err := t.Context().modifyStaticRoutesForNodesWithoutCilium(ctx, "add"); err != nil {
-		return fmt.Errorf("installing static routes: %w", err)
-	}
+	if t.installIPRoutesFromOutsideToPodCIDRs {
+		if err := t.Context().modifyStaticRoutesForNodesWithoutCilium(ctx, "add"); err != nil {
+			return fmt.Errorf("installing static routes: %w", err)
+		}
 
-	t.finalizers = append(t.finalizers, func() error {
-		return t.Context().modifyStaticRoutesForNodesWithoutCilium(ctx, "del")
-	})
+		t.finalizers = append(t.finalizers, func() error {
+			return t.Context().modifyStaticRoutesForNodesWithoutCilium(ctx, "del")
+		})
+	}
 
 	return nil
 }