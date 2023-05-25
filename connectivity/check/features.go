cilium/cilium-cli

commit 3237c9b041d475d62c9f38470c32342486e33066

check: update tunneling detection logic
make the feature detection logic compatible with the new flag introduced
in v1.14 in [1]

[1] https://github.com/cilium/cilium/pull/24561

Signed-off-by: Gilberto Bertin <jibi@cilium.io>

connectivity/check/features.go
@@ -196,13 +196,24 @@ func (ct *ConnectivityTest) extractFeaturesFromConfigMap(ctx context.Context, cl
 		Mode:    mode,
 	}
 
-	mode = "disabled"
-	if v, ok := cm.Data["tunnel"]; ok {
-		mode = v
-	}
-	result[FeatureTunnel] = FeatureStatus{
-		Enabled: mode != "disabled",
-		Mode:    mode,
+	if versioncheck.MustCompile("<1.14.0")(ct.CiliumVersion) {
+		mode = "disabled"
+		if v, ok := cm.Data["tunnel"]; ok {
+			mode = v
+		}
+		result[FeatureTunnel] = FeatureStatus{
+			Enabled: mode != "disabled",
+			Mode:    mode,
+		}
+	} else {
+		mode = "native"
+		if v, ok := cm.Data["routing-mode"]; ok {
+			mode = v
+		}
+		result[FeatureTunnel] = FeatureStatus{
+			Enabled: mode != "native",
+			Mode:    mode,
+		}
 	}
 
 	result[FeatureIPv4] = FeatureStatus{

commit 30319cdf22f1d76f36bed29a28bf27ebb6d3a22f

connectivity: Fix FeatureTunnel.Mode
Set it to --tunnel-protocol on >= 1.14.0. Otherwise, encryption tests
fail to determine a tunnel netdev.

Fixes: 3237c9b04 ("check: update tunneling detection logic")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

connectivity/check/features.go
@@ -210,9 +210,14 @@ func (ct *ConnectivityTest) extractFeaturesFromConfigMap(ctx context.Context, cl
 		if v, ok := cm.Data["routing-mode"]; ok {
 			mode = v
 		}
+		tunnelProto := ""
+		if mode != "native" {
+			tunnelProto = cm.Data["tunnel-protocol"]
+		}
+
 		result[FeatureTunnel] = FeatureStatus{
 			Enabled: mode != "native",
-			Mode:    mode,
+			Mode:    tunnelProto,
 		}
 	}
 