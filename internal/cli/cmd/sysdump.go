cilium/cilium-cli

commit f88a976ee09114d71f6f8519e04a7c783641de96

sysdump: collect SPIRE agent and server Pod logs
Collects the logs of the SPIRE agent & server Pods during sysdump.

Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

internal/cli/cmd/sysdump.go
@@ -65,6 +65,9 @@ func initSysdumpFlags(cmd *cobra.Command, options *sysdump.Options, optionPrefix
 	cmd.Flags().StringVar(&options.CiliumOperatorNamespace,
 		optionPrefix+"cilium-operator-namespace", "",
 		"The namespace Cilium operator is running in")
+	cmd.Flags().StringVar(&options.CiliumSPIRENamespace,
+		optionPrefix+"cilium-spire-namespace", "",
+		"The namespace Cilium SPIRE installation is running in")
 	cmd.Flags().StringVar(&options.CiliumDaemonSetSelector,
 		optionPrefix+"cilium-daemon-set-label-selector", sysdump.DefaultCiliumLabelSelector,
 		"The labels used to target Cilium daemon set")

sysdump/defaults.go
@@ -64,4 +64,8 @@ var (
 	// DefaultCiliumNamespaces will be used to attempt to autodetect what namespace Cilium is installed in
 	// unless otherwise specified.
 	DefaultCiliumNamespaces = []string{"kube-system", "cilium"}
+
+	// DefaultCiliumSPIRENamespaces will be used to attempt to autodetect what namespace Cilium SPIRE is installed in
+	// unless otherwise specified.
+	DefaultCiliumSPIRENamespaces = []string{"kube-system", "cilium", "cilium-spire"}
 )

defaults/defaults.go
@@ -69,6 +69,9 @@ const (
 	ClusterMeshRemoteSecretName           = "clustermesh-apiserver-remote-cert"
 	ClusterMeshExternalWorkloadSecretName = "clustermesh-apiserver-external-workload-cert"
 
+	SPIREServerStatefulSetName = "spire-server"
+	SPIREAgentDaemonSetName    = "spire-agent"
+
 	ConnectivityCheckNamespace = "cilium-test"
 
 	// renovate: datasource=docker

sysdump/sysdump.go
@@ -40,6 +40,8 @@ type Options struct {
 	CiliumNamespace string
 	// The namespace Cilium operator is running in.
 	CiliumOperatorNamespace string
+	// The namespace Cilium SPIRE installation is running in.
+	CiliumSPIRENamespace string
 	// The labels used to target Cilium daemon set. Usually, this label is same as CiliumLabelSelector.
 	CiliumDaemonSetSelector string
 	// The labels used to target Cilium Envoy pods.
@@ -48,6 +50,10 @@ type Options struct {
 	CiliumOperatorLabelSelector string
 	// The labels used to target 'clustermesh-apiserver' pods.
 	ClustermeshApiserverLabelSelector string
+	// The labels used to target Cilium SPIRE server pods.
+	CiliumSPIREServerLabelSelector string
+	// The labels used to target Cilium SPIRE agent pods.
+	CiliumSPIREAgentLabelSelector string
 	// Whether to enable debug logging.
 	Debug bool
 	// Whether to enable scraping profiling data.
@@ -179,6 +185,19 @@ func NewCollector(k KubernetesClient, o Options, startTime time.Time, cliVersion
 		c.Options.CiliumOperatorNamespace = ns
 	} else {
 		c.log("ℹ️  Cilium operator namespace: %s", c.Options.CiliumOperatorNamespace)
+	}
+
+	if c.Options.CiliumSPIRENamespace == "" {
+		if ns, err := detectCiliumSPIRENamespace(k); err != nil {
+			c.logDebug("Failed to detect Cilium SPIRE installation: %v", err)
+			c.log("ℹ️ Failed to detect Cilium SPIRE installation - using Cilium namespace as Cilium SPIRE namespace: %s", c.Options.CiliumOperatorNamespace)
+			c.Options.CiliumSPIRENamespace = c.Options.CiliumOperatorNamespace
+		} else {
+			c.log("🔮 Detected Cilium SPIRE installation in namespace %q", ns)
+			c.Options.CiliumSPIRENamespace = ns
+		}
+	} else {
+		c.log("ℹ️  Cilium SPIRE namespace: %s", c.Options.CiliumSPIRENamespace)
 	}
 
 	// Grab the Kubernetes nodes for the target cluster.
@@ -1112,6 +1131,40 @@ func (c *Collector) Run() error {
 		},
 		{
 			CreatesSubtasks: true,
+			Description:     "Collecting logs from Cilium SPIRE server pods",
+			Quick:           false,
+			Task: func(ctx context.Context) error {
+				p, err := c.Client.ListPods(ctx, c.Options.CiliumSPIRENamespace, metav1.ListOptions{
+					LabelSelector: c.Options.CiliumSPIREServerLabelSelector,
+				})
+				if err != nil {
+					return fmt.Errorf("failed to get logs from Cilium SPIRE server pods")
+				}
+				if err := c.SubmitLogsTasks(FilterPods(p, c.NodeList), c.Options.LogsSinceTime, c.Options.LogsLimitBytes); err != nil {
+					return fmt.Errorf("failed to collect logs from Cilium SPIRE server pods")
+				}
+				return nil
+			},
+		},
+		{
+			CreatesSubtasks: true,
+			Description:     "Collecting logs from Cilium SPIRE agent pods",
+			Quick:           false,
+			Task: func(ctx context.Context) error {
+				p, err := c.Client.ListPods(ctx, c.Options.CiliumSPIRENamespace, metav1.ListOptions{
+					LabelSelector: c.Options.CiliumSPIREAgentLabelSelector,
+				})
+				if err != nil {
+					return fmt.Errorf("failed to get logs from Cilium SPIRE agent pods")
+				}
+				if err := c.SubmitLogsTasks(FilterPods(p, c.NodeList), c.Options.LogsSinceTime, c.Options.LogsLimitBytes); err != nil {
+					return fmt.Errorf("failed to collect logs from Cilium SPIRE agent pods")
+				}
+				return nil
+			},
+		},
+		{
+			CreatesSubtasks: true,
 			Description:     "Collecting platform-specific data",
 			Quick:           true,
 			Task: func(ctx context.Context) error {
@@ -2007,4 +2060,27 @@ func detectCiliumOperatorNamespace(k KubernetesClient) (string, error) {
 		return ns.Name, nil
 	}
 	return "", fmt.Errorf("failed to detect Cilium operator namespace, could not find Cilium installation in namespaces: %v", DefaultCiliumNamespaces)
+}
+
+func detectCiliumSPIRENamespace(k KubernetesClient) (string, error) {
+	for _, ns := range DefaultCiliumSPIRENamespaces {
+		ctx := context.Background()
+		ns, err := k.GetNamespace(ctx, ns, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			continue
+		}
+		if err != nil {
+			return "", fmt.Errorf("failed to detect Cilium SPIRE namespace: %w", err)
+		}
+
+		_, err = k.GetDaemonSet(ctx, ns.Name, ciliumSPIREAgentDaemonSetName, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			continue
+		}
+		if err != nil {
+			return "", fmt.Errorf("failed to check for Cilium SPIRE agent DaemonSet: %w", err)
+		}
+		return ns.Name, nil
+	}
+	return "", fmt.Errorf("failed to detect Cilium SPIRE namespace, could not find Cilium SPIRE installation in namespaces: %v", DefaultCiliumSPIRENamespaces)
 }

sysdump/constants.go
@@ -19,6 +19,8 @@ const (
 	ciliumConfigMapName                = defaults.ConfigMapName
 	ciliumEtcdSecretsSecretName        = "cilium-etcd-secrets"
 	ciliumOperatorDeploymentName       = defaults.OperatorDeploymentName
+	ciliumSPIREServerStatefulSetName   = defaults.SPIREServerStatefulSetName
+	ciliumSPIREAgentDaemonSetName      = defaults.SPIREAgentDaemonSetName
 	clustermeshApiserverDeploymentName = defaults.ClusterMeshDeploymentName
 	hubbleContainerName                = "hubble"
 	hubbleDaemonSetName                = "hubble"

commit 632f0f8c5e3b3f6cf4f37bb59ef11cfa148b56e6

sysdump: Specify default selectors for log collection tasks
Specify default label selectors for log collection tasks to avoid
collecting logs from all the pods in a given namespace.

Fixes: f88a976e ("sysdump: collect SPIRE agent and server Pod logs")
Fixes: ebaf4fbd ("sysdump: Collect node init Pod logs and DaemonSet")

Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

internal/cli/cmd/sysdump.go
@@ -83,6 +83,15 @@ func initSysdumpFlags(cmd *cobra.Command, options *sysdump.Options, optionPrefix
 	cmd.Flags().StringVar(&options.ClustermeshApiserverLabelSelector,
 		optionPrefix+"clustermesh-apiserver-label-selector", sysdump.DefaultClustermeshApiserverLabelSelector,
 		"The labels used to target 'clustermesh-apiserver' pods")
+	cmd.Flags().StringVar(&options.CiliumNodeInitLabelSelector,
+		optionPrefix+"cilium-node-init-selector", sysdump.DefaultCiliumNodeInitLabelSelector,
+		"The labels used to target Cilium node init pods")
+	cmd.Flags().StringVar(&options.CiliumSPIREAgentLabelSelector,
+		optionPrefix+"cilium-spire-agent-selector", sysdump.DefaultCiliumSpireAgentLabelSelector,
+		"The labels used to target Cilium spire-agent pods")
+	cmd.Flags().StringVar(&options.CiliumSPIREServerLabelSelector,
+		optionPrefix+"cilium-spire-server-selector", sysdump.DefaultCiliumSpireServerLabelSelector,
+		"The labels used to target Cilium spire-server pods")
 	cmd.Flags().BoolVar(&options.Debug,
 		optionPrefix+"debug", sysdump.DefaultDebug,
 		"Whether to enable debug logging")

sysdump/defaults.go
@@ -19,6 +19,9 @@ const (
 	DefaultCiliumEnvoyLabelSelector          = labelPrefix + "cilium-envoy"
 	DefaultCiliumOperatorLabelSelector       = "io.cilium/app=operator"
 	DefaultClustermeshApiserverLabelSelector = labelPrefix + "clustermesh-apiserver"
+	DefaultCiliumNodeInitLabelSelector       = "app=cilium-node-init"
+	DefaultCiliumSpireAgentLabelSelector     = "app=spire-agent"
+	DefaultCiliumSpireServerLabelSelector    = "app=spire-server"
 	DefaultDebug                             = false
 	DefaultProfiling                         = true
 	DefaultHubbleLabelSelector               = labelPrefix + "hubble"