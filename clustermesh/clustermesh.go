cilium/cilium-cli

commit 1e059407dc39c05ba34042e92702e8d9dacf7c77

clustermesh: Adapt clustermesh script to install cilium-dbg
Use the 'cilium' binary from a Cilium container to copy into
/usr/bin/cilium and /usr/bin/cilium-dbg so that this script will be
compatible with newer CI workflows in future that rely on running
cilium-dbg commands rather than cilium commands directly within the Pod.

The 'cp -L' symbolic link dereferencing will make this compatible with
newer v1.15-dev based images.

Signed-off-by: Joe Stringer <joe@cilium.io>

clustermesh/clustermesh.go
@@ -1605,9 +1605,13 @@ if [ "$1" = "uninstall" ] ; then
         echo "Shutting down running Cilium agent"
         ${SUDO} docker rm -f cilium || true
     fi
-    if [ -f /usr/bin/cilium ] ; then
+    if [ -e /usr/bin/cilium ]; then
         echo "Removing /usr/bin/cilium"
         ${SUDO} rm /usr/bin/cilium
+    fi
+    if [ -e /usr/bin/cilium-dbg ] ; then
+        echo "Removing /usr/bin/cilium-dbg"
+        ${SUDO} rm /usr/bin/cilium-dbg
     fi
     pushd /etc
     if [ -f resolv.conf.orig ] ; then
@@ -1710,11 +1714,12 @@ while [ $cilium_started = false ]; do
     ${SUDO} docker run --name cilium $DOCKER_OPTS $CILIUM_IMAGE cilium-agent $CILIUM_OPTS
 
     # Copy Cilium CLI
-    ${SUDO} docker cp cilium:/usr/bin/cilium /usr/bin/cilium
+    ${SUDO} docker cp -L cilium:/usr/bin/cilium /usr/bin/cilium-dbg
+    ${SUDO} ln -s /usr/bin/cilium-dbg /usr/bin/cilium
 
     # Wait for cilium agent to become available
     for ((i = 0 ; i < 12; i++)); do
-        if ${SUDO} cilium status --brief > /dev/null 2>&1; then
+        if ${SUDO} cilium-dbg status --brief > /dev/null 2>&1; then
             cilium_started=true
             break
         fi
@@ -1723,7 +1728,7 @@ while [ $cilium_started = false ]; do
     done
 
     echo "Cilium status:"
-    ${SUDO} cilium status || true
+    ${SUDO} cilium-dbg status || true
 
     if [ "$cilium_started" = true ] ; then
         echo 'Cilium successfully started!'
@@ -1740,7 +1745,7 @@ done
 # Wait for kube-dns service to become available
 kubedns=""
 for ((i = 0 ; i < 24; i++)); do
-    kubedns=$(${SUDO} cilium service list get -o jsonpath='{[?(@.spec.frontend-address.port==53)].spec.frontend-address.ip}')
+    kubedns=$(${SUDO} cilium-dbg service list get -o jsonpath='{[?(@.spec.frontend-address.port==53)].spec.frontend-address.ip}')
     if [ -n "$kubedns" ] ; then
         break
     fi
@@ -1748,7 +1753,7 @@ for ((i = 0 ; i < 24; i++)); do
     echo "Waiting for kube-dns service to come available..."
 done
 
-namespace=$(${SUDO} cilium endpoint get -l reserved:host -o jsonpath='{$[0].status.identity.labels}' | tr -d "[]\"" | tr "," "\n" | grep io.kubernetes.pod.namespace | cut -d= -f2)
+namespace=$(${SUDO} cilium-dbg endpoint get -l reserved:host -o jsonpath='{$[0].status.identity.labels}' | tr -d "[]\"" | tr "," "\n" | grep io.kubernetes.pod.namespace | cut -d= -f2)
 
 if [ -n "$kubedns" ] ; then
     if grep "nameserver $kubedns" /etc/resolv.conf ; then

commit aa1077a148cb273f13800b9db456bb3a46086285

clustermesh: Make cilium symlink install idempotent
Ensure the symlink is installed even if it already exists.

Fixes: 1e059407dc39 ("clustermesh: Adapt clustermesh script to install cilium-dbg")
Signed-off-by: Joe Stringer <joe@cilium.io>

clustermesh/clustermesh.go
@@ -1715,7 +1715,7 @@ while [ $cilium_started = false ]; do
 
     # Copy Cilium CLI
     ${SUDO} docker cp -L cilium:/usr/bin/cilium /usr/bin/cilium-dbg
-    ${SUDO} ln -s /usr/bin/cilium-dbg /usr/bin/cilium
+    ${SUDO} ln -fs /usr/bin/cilium-dbg /usr/bin/cilium
 
     # Wait for cilium agent to become available
     for ((i = 0 ; i < 12; i++)); do