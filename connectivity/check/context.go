cilium/cilium-cli

commit 89c7ff04254fb6b6860b9c136cd04a93ef16cc8c

connectivity: Add GetTestOrDie()
Add a convenient function GetTestOrDie(). There isn't much you can do if
GetTest() fails anyways.

Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

connectivity/check/context.go
@@ -293,6 +293,16 @@ func (ct *ConnectivityTest) GetTest(name string) (*Test, error) {
 	panic("missing test descriptor for a registered name")
 }
 
+// GetTestOrDie returns the test scope for test named "name" if found,
+// or panics otherwise.
+func (ct *ConnectivityTest) GetTestOrDie(name string) *Test {
+	test, err := ct.GetTest(name)
+	if err != nil {
+		panic(err)
+	}
+	return test
+}
+
 // SetupAndValidate sets up and validates the connectivity test infrastructure
 // such as the client pods and validates the deployment of them along with
 // Cilium. This must be run before Run() is called.

commit 69e6ab9e30d6d0ec42431a578374aa1232799e97

Rename GetTestOrDie to MustGetTest
Fixes: 89c7ff04254f ("connectivity: Add GetTestOrDie()")

Ref: https://github.com/cilium/cilium-cli/pull/2096#pullrequestreview-1721958513

Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

connectivity/check/context.go
@@ -293,9 +293,9 @@ func (ct *ConnectivityTest) GetTest(name string) (*Test, error) {
 	panic("missing test descriptor for a registered name")
 }
 
-// GetTestOrDie returns the test scope for test named "name" if found,
+// MustGetTest returns the test scope for test named "name" if found,
 // or panics otherwise.
-func (ct *ConnectivityTest) GetTestOrDie(name string) *Test {
+func (ct *ConnectivityTest) MustGetTest(name string) *Test {
 	test, err := ct.GetTest(name)
 	if err != nil {
 		panic(err)