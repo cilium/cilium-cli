cilium/cilium-cli

commit 9ddd6d82c29791e219ed3eb72eac11c626baaf66

install: Do not use individual HelmOpts
We need to merge the Helm opts before iterating over them, as they can
be defined via different fields.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

install/autodetect.go
@@ -190,8 +190,7 @@ func (k *K8sInstaller) autodetectAndValidate(ctx context.Context) error {
 	}
 
 	k.autodetectKubeProxy(ctx)
-	k.autoEnableBPFMasq()
-	return nil
+	return k.autoEnableBPFMasq()
 }
 
 func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context) error {
@@ -261,12 +260,22 @@ func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context) error {
 	return nil
 }
 
-func (k *K8sInstaller) autoEnableBPFMasq() {
+func (k *K8sInstaller) autoEnableBPFMasq() error {
+	vals, err := k.getHelmValues()
+	if err != nil {
+		return err
+	}
+
 	// Auto-enable BPF masquerading if KPR=strict and IPv6=disabled
 	foundKPRStrict := k.params.KubeProxyReplacement == "strict"
 	foundMasq := false
 	enabledIPv6 := false
-	for _, param := range k.params.HelmOpts.Values {
+	for _, param := range vals {
+		param, ok := param.(string)
+		if !ok {
+			continue
+		}
+
 		if !foundKPRStrict && param == "kubeProxyReplacement=strict" {
 			foundKPRStrict = true
 			continue
@@ -285,4 +294,6 @@ func (k *K8sInstaller) autoEnableBPFMasq() {
 		k.params.HelmOpts.Values = append(k.params.HelmOpts.Values,
 			"bpf.masquerade=true")
 	}
+
+	return nil
 }

commit 40c58c4f6fd12bd9e1b7e00c3818ff10c916fa4b

helm: remove bpf.masquerade autodetection logic
The current logic is supposed to automatically enable BPF masquerade
when KPR is enabled, and IPv6 is not enabled. However, it looks like
it is broken since 9ddd6d82c297 ("install: Do not use individual
HelmOpts"), because we iterate on a map of maps containing the rendered
values, but attempt to match the values against --set strings.

Instead of fixing it, let's just remove this logic altogether for the
sake of simplicity. Additionally, it is potentially fragile, and may
lead to surprises to users (as well as in CI) given the difference with
respect to the default Cilium's values, and a plain helm install.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

install/autodetect.go
@@ -157,7 +157,7 @@ func (k *K8sInstaller) autodetectAndValidate(ctx context.Context, helmValues map
 	}
 
 	k.autodetectKubeProxy(ctx)
-	return k.autoEnableBPFMasq()
+	return nil
 }
 
 func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context) error {
@@ -220,44 +220,6 @@ func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context) error {
 			"kubeProxyReplacement=strict",
 			fmt.Sprintf("k8sServiceHost=%s", apiServerHost),
 			fmt.Sprintf("k8sServicePort=%s", apiServerPort))
-	}
-
-	return nil
-}
-
-func (k *K8sInstaller) autoEnableBPFMasq() error {
-	vals, err := k.getHelmValues()
-	if err != nil {
-		return err
-	}
-
-	// Auto-enable BPF masquerading if KPR=strict and IPv6=disabled
-	foundKPRStrict := false
-	foundMasq := false
-	enabledIPv6 := false
-	for _, param := range vals {
-		param, ok := param.(string)
-		if !ok {
-			continue
-		}
-
-		if !foundKPRStrict && param == "kubeProxyReplacement=strict" {
-			foundKPRStrict = true
-			continue
-		}
-		if strings.HasPrefix(param, "bpf.masquerade") {
-			foundMasq = true
-			break
-		}
-		if strings.HasPrefix(param, "ipv6.enabled=true") {
-			enabledIPv6 = true
-			break
-		}
-	}
-
-	if foundKPRStrict && !foundMasq && !enabledIPv6 {
-		k.params.HelmOpts.Values = append(k.params.HelmOpts.Values,
-			"bpf.masquerade=true")
 	}
 
 	return nil