cilium/cilium-cli

commit 918ae165b6995780e3f57aa003d6c888ccf3cecd

ci: avoid overlapping pod and service CIDRs on AKS
Follow https://github.com/cilium/cilium/pull/31504, quoting its commit
description:

> The default service CIDR of AKS clusters is 10.0.0.0/16 [1].
> Unfortunately, we don't set a pod cidr for clusterpool IPAM, and hence
> use cilium's default of 10.0.0.0/8, which overlaps. This can
> lead to "fun" situations in which e.g. the kube-dns service ClusterIP is
> the same as the hubble-relay pod IP, or similar shenanigans. This
> usually breaks the cluster utterly.
>
> The fix is relatively straight-forward: set a pod CIDR for cilium which
> does not overlap with defaults of AKS. We chose 192.168.0.0/16 as this
> is what is recommended in [2].
>
> [1]: https://learn.microsoft.com/en-us/azure/aks/configure-kubenet#create-an-aks-cluster-with-system-assigned-managed-identities
> [2]: https://learn.microsoft.com/en-us/azure/aks/azure-cni-powered-by-cilium#option-1-assign-ip-addresses-from-an-overlay-network

Co-authored-by: Fabian Fischer <fabian.fischer@isovalent.com>
Co-authored-by: David Bimmler <david.bimmler@isovalent.com>
Signed-off-by: Tobias Klauser <tobias@cilium.io>

.github/in-cluster-test-scripts/aks-byocni-install.sh
@@ -10,4 +10,5 @@ cilium install \
   --wait=false \
   --set loadBalancer.l7.backend=envoy \
   --set tls.secretsBackend=k8s \
-  --set bpf.monitorAggregation=none
+  --set bpf.monitorAggregation=none \
+  --set ipam.operator.clusterPoolIPv4PodCIDRList=192.168.0.0/16" # To avoid clashing with the default Service CIDR of AKS (10.0.0.0/16)

commit 06818b87894716f417b397672d8e0cce54d43d41

ci: fix installation on AKS
The trailing " makes the installation fail on AKS with:

2024-03-26T19:25:46.385719258Z /root/test/in-cluster-test-script.sh: line 15: syntax error: unterminated quoted string

See e.g. https://github.com/cilium/cilium-cli/actions/runs/8440743477/job/23118350044

Fixes: 918ae165b699 ("ci: avoid overlapping pod and service CIDRs on AKS")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

.github/in-cluster-test-scripts/aks-byocni-install.sh
@@ -11,4 +11,4 @@ cilium install \
   --set loadBalancer.l7.backend=envoy \
   --set tls.secretsBackend=k8s \
   --set bpf.monitorAggregation=none \
-  --set ipam.operator.clusterPoolIPv4PodCIDRList=192.168.0.0/16" # To avoid clashing with the default Service CIDR of AKS (10.0.0.0/16)
+  --set ipam.operator.clusterPoolIPv4PodCIDRList=192.168.0.0/16 # To avoid clashing with the default Service CIDR of AKS (10.0.0.0/16)