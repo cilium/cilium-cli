cilium/cilium-cli

commit ad3a82c210eec0c166fd7727b26b3c4ec80da85e

helm: improve KPR autodetection logic
Make sure that the autodetected values don't override possible user
configurations. Additionally, adapt the kubeProxyReplacement value
based on the CIlium version, as strict got deprevated in v1.14.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

install/autodetect.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/cilium/cilium-cli/k8s"
 
+	"github.com/cilium/cilium/pkg/versioncheck"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -138,11 +139,11 @@ func (k *K8sInstaller) autodetectAndValidate(ctx context.Context, helmValues map
 		return err
 	}
 
-	k.autodetectKubeProxy(ctx)
+	k.autodetectKubeProxy(ctx, helmValues)
 	return nil
 }
 
-func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context) error {
+func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context, helmValues map[string]interface{}) error {
 	if k.flavor.Kind == k8s.KindK3s {
 		return nil
 	}
@@ -197,11 +198,25 @@ func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context) error {
 	if apiServerHost != "" && apiServerPort != "" {
 		k.Log("🔮 Auto-detected kube-proxy has not been installed")
 		k.Log("ℹ️  Cilium will fully replace all functionalities of kube-proxy")
+
+		setIfUnset := func(key, value string) {
+			_, found, _ := unstructured.NestedFieldNoCopy(helmValues, key)
+			if !found {
+				k.params.HelmOpts.Values = append(k.params.HelmOpts.Values,
+					fmt.Sprintf("%s=%s", key, value))
+			}
+		}
+
 		// Use HelmOpts to set auto kube-proxy installation
-		k.params.HelmOpts.Values = append(k.params.HelmOpts.Values,
-			"kubeProxyReplacement=strict",
-			fmt.Sprintf("k8sServiceHost=%s", apiServerHost),
-			fmt.Sprintf("k8sServicePort=%s", apiServerPort))
+		setIfUnset("kubeProxyReplacement", func() string {
+			if !versioncheck.MustCompile(">=1.14.0")(k.chartVersion) {
+				return "true"
+			}
+			return "strict"
+		}())
+
+		setIfUnset("k8sServiceHost", apiServerHost)
+		setIfUnset("k8sServicePort", apiServerPort)
 	}
 
 	return nil

commit 9735b7dcaaefea71050dc208adc26be7826d3e0a

helm: fix KPR autodetection value
The logic to determine whether KPR should be enabled using the "true"
setting or the legacy "strict" one, based on the Cilium version, appears
to be inverted, apparently due to a test change which got incorrectly
committed. Let's fix it.

Fixes: ad3a82c210ee ("helm: improve KPR autodetection logic")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

install/autodetect.go
@@ -209,7 +209,7 @@ func (k *K8sInstaller) autodetectKubeProxy(ctx context.Context, helmValues map[s
 
 		// Use HelmOpts to set auto kube-proxy installation
 		setIfUnset("kubeProxyReplacement", func() string {
-			if !versioncheck.MustCompile(">=1.14.0")(k.chartVersion) {
+			if versioncheck.MustCompile(">=1.14.0")(k.chartVersion) {
 				return "true"
 			}
 			return "strict"