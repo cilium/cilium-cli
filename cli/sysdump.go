cilium/cilium-cli

commit aad7a72065fa52cfaae6c564e462299865e3dc82

Support operator running in a different namespace
Signed-off-by: Jared Ledvina <jared.ledvina@datadoghq.com>

sysdump/sysdump.go
@@ -34,6 +34,8 @@ type Options struct {
 	CiliumLabelSelector string
 	// The namespace Cilium is running in.
 	CiliumNamespace string
+	// The namespace Cilium operator is running in.
+	CiliumOperatorNamespace string
 	// The labels used to target Cilium daemon set. Usually, this label is same as CiliumLabelSelector.
 	CiliumDaemonSetSelector string
 	// The labels used to target Cilium operator pods.
@@ -557,7 +559,7 @@ func (c *Collector) Run() error {
 			Description: "Collecting the Cilium operator deployment",
 			Quick:       true,
 			Task: func(ctx context.Context) error {
-				v, err := c.Client.GetDeployment(ctx, c.Options.CiliumNamespace, ciliumOperatorDeploymentName, metav1.GetOptions{})
+				v, err := c.Client.GetDeployment(ctx, c.Options.CiliumOperatorNamespace, ciliumOperatorDeploymentName, metav1.GetOptions{})
 				if err != nil {
 					return fmt.Errorf("failed to collect the Cilium operator deployment: %w", err)
 				}

internal/cli/cmd/sysdump.go
@@ -52,6 +52,9 @@ func newCmdSysdump() *cobra.Command {
 	cmd.Flags().StringVar(&sysdumpOptions.CiliumNamespace,
 		"cilium-namespace", sysdump.DefaultCiliumNamespace,
 		"The namespace Cilium is running in")
+	cmd.Flags().StringVar(&sysdumpOptions.CiliumOperatorNamespace,
+		"cilium-operator-namespace", sysdump.DefaultCiliumNamespace,
+		"The namespace Cilium operator is running in")
 	cmd.Flags().StringVar(&sysdumpOptions.CiliumDaemonSetSelector,
 		"cilium-daemon-set-label-selector", sysdump.DefaultCiliumLabelSelector,
 		"The labels used to target Cilium daemon set")

commit 3868adda8185106d4cf48b63fbadbbe1b2277d5d

sysdump: Check --namespace flag for cilium-operator
Use --namespace flag for cilium-operator if --cilium-operator-namespace
flag is not specified. Otherwise sysdump doesn't execute Cilium-related
tasks [^1].

[^1]: https://github.com/cilium/cilium-cli/blob/c29afa4bbe93f90531b7611847f7703db603a27c/sysdump/sysdump.go#L1362

Fixes: aad7a72065fa ("Support operator running in a different namespace")
Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

cli/sysdump.go
@@ -34,6 +34,9 @@ func newCmdSysdump(hooks sysdump.Hooks) *cobra.Command {
 			if sysdumpOptions.CiliumNamespace == "" && cmd.Flags().Changed("namespace") {
 				sysdumpOptions.CiliumNamespace = namespace
 			}
+			if sysdumpOptions.CiliumOperatorNamespace == "" && cmd.Flags().Changed("namespace") {
+				sysdumpOptions.CiliumOperatorNamespace = namespace
+			}
 			// Honor --helm-release-name global flag in case it is set and --cilium-helm-release-name is not set
 			if sysdumpOptions.CiliumHelmReleaseName == "" && cmd.Flags().Changed("helm-release-name") {
 				sysdumpOptions.CiliumHelmReleaseName = helmReleaseName

sysdump/sysdump.go
@@ -2816,7 +2816,7 @@ func InitSysdumpFlags(cmd *cobra.Command, options *Options, optionPrefix string,
 		"The namespace Cilium is running in. If not provided then the --namespace global flag is used (if provided)")
 	cmd.Flags().StringVar(&options.CiliumOperatorNamespace,
 		optionPrefix+"cilium-operator-namespace", "",
-		"The namespace Cilium operator is running in")
+		"The namespace Cilium operator is running in. If not provided then the --namespace global flag is used (if provided)")
 	cmd.Flags().StringVar(&options.CiliumSPIRENamespace,
 		optionPrefix+"cilium-spire-namespace", "",
 		"The namespace Cilium SPIRE installation is running in")