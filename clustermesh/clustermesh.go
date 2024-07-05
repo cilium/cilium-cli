cilium/cilium-cli

commit b148800cf7a27a12861217dcced031f6d8b16c62

clustermesh: reset remote clusters configuration upon disconnection
cilium/cilium#28763 decoupled the helm settings to enable the
clustermesh-apiserver and provide the list of clusters to connect to.
Let's reflect this change to the 'cilium clustermesh disable' command
as well, explicitly disabling and resetting the remote clusters configs
when invoked, to correctly disconnect from possible leftover clusters.

Fixes: cilium/cilium#32300
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

clustermesh/clustermesh.go
@@ -1521,6 +1521,8 @@ func EnableWithHelm(ctx context.Context, k8sClient *k8s.Client, params Parameter
 func DisableWithHelm(ctx context.Context, k8sClient *k8s.Client, params Parameters) error {
 	helmStrValues := []string{
 		"clustermesh.useAPIServer=false",
+		"clustermesh.config.enabled=false",
+		"clustermesh.config.clusters=null",
 		"externalWorkloads.enabled=false",
 	}
 	vals, err := helm.ParseVals(helmStrValues)

commit 1b51aba1ca547ddb9eb6ddb2236a2ce214f06502

clustermesh: fix remote clusters configuration reset upon deactivation
The blamed commit explicitly reset the list of remote clusters upon
clustermesh deactivation. However, it did so setting the corresponding
setting to nil, which in Cilium v1.16 triggers an error due to not
matching the schema configured. Let's address this by configuring an
empty array instead.

Fixes: b148800cf7a2 ("clustermesh: reset remote clusters configuration upon disconnection")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

clustermesh/clustermesh.go
@@ -1522,13 +1522,18 @@ func DisableWithHelm(ctx context.Context, k8sClient *k8s.Client, params Paramete
 	helmStrValues := []string{
 		"clustermesh.useAPIServer=false",
 		"clustermesh.config.enabled=false",
-		"clustermesh.config.clusters=null",
 		"externalWorkloads.enabled=false",
 	}
 	vals, err := helm.ParseVals(helmStrValues)
 	if err != nil {
 		return err
 	}
+
+	err = unstructured.SetNestedSlice(vals, []interface{}{}, "clustermesh", "config", "clusters")
+	if err != nil {
+		return err
+	}
+
 	upgradeParams := helm.UpgradeParameters{
 		Namespace:   params.Namespace,
 		Name:        params.HelmReleaseName,