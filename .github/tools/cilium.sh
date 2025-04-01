9d070509ab3c53936e79418abca1979c42dc4900
Michi Mutsuzaki <michi@isovalent.com>
2025-04-01 21:46:38 +0000

    action: Add kubeconfig-mount-path input
    Parameterize the mount path for kubeconfig to make it easier to migrate
    to distroless nonroot image. Also delete unused volume mounts.
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- .github/tools/cilium.sh
+++ .github/tools/cilium.sh
@@ -5,13 +5,11 @@ set -ex
 CILIUM_CLI_IMAGE_REPO=${CILIUM_CLI_IMAGE_REPO:-quay.io/cilium/cilium-cli-ci}
 CILIUM_CLI_IMAGE_TAG=${CILIUM_CLI_IMAGE_TAG:-latest}
 KUBECONFIG=${KUBECONFIG:-~/.kube/config}
+KUBECONFIG_MOUNT_PATH=${KUBECONFIG_MOUNT_PATH:-/root/.kube/config}
 
 docker run \
   --network host \
-  -v "$KUBECONFIG":/root/.kube/config \
+  -v "$KUBECONFIG":"$KUBECONFIG_MOUNT_PATH" \
   -v "$(pwd)":/root/app \
-  -v ~/.aws:/root/.aws \
-  -v ~/.azure:/root/.azure \
-  -v ~/.config/gcloud:/root/.config/gcloud \
   -e GITHUB_WORKFLOW_REF="$GITHUB_WORKFLOW_REF" \
   "$CILIUM_CLI_IMAGE_REPO":"$CILIUM_CLI_IMAGE_TAG" cilium "$@"
